8e110c25f9892714963b4eece60c150ea69b3215
Priyanshu <Preyanshukandari@gmail.com>
2025-09-18 15:26:24 +0530

    JSON completed

---
+++ Day02/JSON/app.js
@@ -0,0 +1,7 @@
+const obj = {name:"harkirat",age:24,gender:"male"}
+const finalstring = JSON.stringify(obj)
+console.log(finalstring);
+
+const str = '{"name":"harkirat","age":24,"gender":"male"}'
+const finalstring1 = JSON.parse(str)
+console.log(finalstring1);
