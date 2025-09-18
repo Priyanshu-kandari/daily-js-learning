123588ff39a8f9073b2f58f4788b21b4aea05f25
Priyanshu <Preyanshukandari@gmail.com>
2025-09-18 13:04:18 +0530

    small changes

---
+++ Day02/numbers/app.js
@@ -0,0 +1,11 @@
+
+//NUMBERS--------------------------------------------------------------------------------------------
+
+//parseint
+console.log(parseInt("42"));
+console.log(parseInt("42px"));
+console.log(parseInt("4.12"));
+
+console.log(parseFloat("42.5"));
+console.log(parseFloat("42.2px"));
+console.log(parseFloat("4.12"));
