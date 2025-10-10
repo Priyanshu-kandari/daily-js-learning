f85c622abc6c02731533b100e0ac96721f4772dd
Priyanshu <Preyanshukandari@gmail.com>
2025-10-10 10:54:36 +0530

    completed

---
+++ Day01/LetConstVar/app.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountCity])
+
+// accountId = 2 *not allowed
+
+accountEmail = "priyanshu@gmail.com"
+accountpassword = "21212121"
+accountCity = "bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountCity])
