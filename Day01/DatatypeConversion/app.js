be56d8871536291528fc843826ee566de4b33b8b
Priyanshu <Preyanshukandari@gmail.com>
2025-10-10 18:05:38 +0530

    completed

---
+++ Day01/DatatypeConversion/app.js
@@ -0,0 +1,95 @@
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score);
+// console.log(valueInNumber);
+
+// string
+// string
+// number
+// 33
+// 33
+
+
+
+
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score);
+// console.log(valueInNumber);
+
+// string
+// string
+// number
+// 33abc
+// NaN
+
+
+
+// let score = true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score);
+// console.log(valueInNumber);
+
+// boolean
+// boolean
+// number
+// true
+// 1
+
+
+
+// let score = null
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(score);
+// console.log(valueInNumber);
+
+// object
+// object
+// number
+// null
+// 0
+
+
+// let isloggedin = 1
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+//true
+
+
+// let isloggedin = ""
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+// false
+
+
+
+// let isloggedin = "hitesh"
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+// true
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+//string
