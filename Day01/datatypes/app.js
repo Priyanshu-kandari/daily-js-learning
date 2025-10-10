3e0a5bb5235d587f8cf17a380085db8cda753a7a
Priyanshu <Preyanshukandari@gmail.com>
2025-10-10 11:09:25 +0530

    completed

---
+++ Day01/datatypes/app.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  *we are using nodeJS, not browser
+
+
+let name = "Priyanshu"
+let age = 18
+let isloggedin = false
+
+//number => 2^53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => Unique
+
+
+//objects
+console.log(typeof age);  //number
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
