2e4e893649d6dd941e84ef1665408c178eb45000
Priyanshu <Preyanshukandari@gmail.com>
2025-10-11 13:56:52 +0530

    completed

---
+++ Day01/stringToNumber/app.js
@@ -0,0 +1,21 @@
+let value = 33
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " Priyanshu"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 + "2");  //32
+
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+let counter = 100
+++counter
+console.log(counter);
